71b392bd3960cbe8c8642cbc7150efa8061f035d
Yaroslav Sviridov <ys9617890962@gmail.com>
2020-10-17 02:37:14 +0300

    Add fibonacci method and testing functions

--- HW1/HW1/Program.cs
+++ HW1/HW1/Program.cs
@@ -4,29 +4,19 @@ using System.Diagnostics;
 
 namespace HW1
 {
-    public abstract class OptimisationMethod
+    public abstract class OptimizationMethod
     {
         protected readonly double Epsilon;
-        protected OptimisationMethod(double epsilon)
+        protected OptimizationMethod(double epsilon)
         {
             Epsilon = epsilon;
         }
 
-        public abstract (double, double) FindMinimum(Func<double, double> f, double l, double r);
+        public abstract (double X, double Y) FindMinimum(Func<double, double> f, double l, double r);
 
         protected void UpdateInterval(ref double l, ref double r, double x1, double x2, double y1, double y2)
         {
-            if (x1 > x2)
-            {
-                throw new ArgumentOutOfRangeException(nameof(x1) + ", " + nameof(x2), "x1 should be less than x2");
-            }
-            
-            if (Math.Abs(y1 - y2) < Epsilon)
-            {
-                l = x1;
-                r = x2;
-            }
-            else if (y1 > y2)
+            if (y1 > y2)
             {
                 l = x1;
             }
@@ -37,17 +27,12 @@ namespace HW1
         }
     }
 
-    public class DichotomyMethod : OptimisationMethod
+    public class DichotomyMethod : OptimizationMethod
     {
         public DichotomyMethod(double epsilon) : base(epsilon) { }
         
-        public override (double, double) FindMinimum(Func<double, double> f, double l, double r)
+        public override (double X, double Y) FindMinimum(Func<double, double> f, double l, double r)
         {
-            if (l > r)
-            {
-                throw new ArgumentOutOfRangeException(nameof(l) + ", " + nameof(r), "l should be less than r");
-            }
-
             double delta = Epsilon / 4;
             while (Math.Abs(r - l) > Epsilon)
             {
@@ -63,23 +48,23 @@ namespace HW1
             double answerY = f(answerX);
             return (answerX, answerY);
         }
-        
+
+        public override string ToString()
+        {
+            return "DichotomyMethod";
+        }
     }
 
-    public class GoldenSectionMethod : OptimisationMethod
+    public class GoldenSectionMethod : OptimizationMethod
     {
         private const double F = 1.618033988749894848;
 
         public GoldenSectionMethod(double epsilon) : base(epsilon) { }
 
-        public override (double, double) FindMinimum(Func<double, double> f, double l, double r)
+        public override (double X, double Y) FindMinimum(Func<double, double> f, double l, double r)
         {
             Dictionary<double, double> cache = new Dictionary<double, double>();
-            if (l > r)
-            {
-                throw new ArgumentOutOfRangeException(nameof(l) + ", " + nameof(r), "l should be less than r");
-            }
-            
+
             while (Math.Abs(r - l) > Epsilon)
             {
                 double x1 = r - (r - l) / F, 
@@ -102,13 +87,111 @@ namespace HW1
                 return cache[x];
             }
         }
+        
+        public override string ToString()
+        {
+            return "GoldenSectionMethod";
+        }
+    }
+
+    public class FibonacciMethod : OptimizationMethod
+    {
+        public FibonacciMethod(double epsilon) : base(epsilon) { }
+
+        public override (double X, double Y) FindMinimum(Func<double, double> f, double l, double r)
+        {
+            Dictionary<double, double> cache = new Dictionary<double, double>();
+
+            double f1 = 0, f2 = 1, f3 = 1;
+            while (f3 <= (r - l) / Epsilon)
+            {
+                f1 = f2;
+                f2 = f3;
+                f3 = f1 + f2;
+            }
+
+            double x1 = l + f1 / f3 * (r - l);
+            double x2 = l + f2 / f3 * (r - l);
+            double y1, y2;
+
+            while (Math.Abs(r - l) > Epsilon)
+            {
+                y1 = Ask(x1);
+                y2 = Ask(x2);
+                UpdateInterval(ref l, ref r, ref x1, ref x2, y1, y2);
+            }
+            
+            double answerX = (r + l) / 2;
+            double answerY = f(answerX);
+            return (answerX, answerY);
+            
+            double Ask(double x)
+            {
+                if (!cache.ContainsKey(x))
+                {
+                    cache[x] = f(x);
+                }
+                return cache[x];
+            }
+        }
+
+        void UpdateInterval(ref double l, ref double r, ref double x1, ref double x2, double y1, double y2)
+        {
+            if (y1 > y2)
+            {
+                l = x1;
+                x1 = l + (r - x2);
+            }
+            else
+            {
+                r = x2;
+                x2 = r - (x1 - l);
+            }
+
+            if (x1 > x2)
+            {
+                (x1, x2) = (x2, x1);
+            }
+        }
+        
+        public override string ToString()
+        {
+            return "FibonacciMethod";
+        }
     }
     
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            (double l, double r, Func<double, double> f)[] queries =
+            {
+                (-0.5, 0.5,
+                    x => -5 * Math.Pow(x, 5) + 4 * Math.Pow(x, 4) - 12 * Math.Pow(x, 3) + 11 * Math.Pow(x, 2) - 2 * x + 1),
+                (6, 9.9,
+                    x => Math.Pow(Math.Log10(x - 2), 2) + Math.Pow(Math.Log10(10 - x), 2) - Math.Pow(x, 0.2)),
+                (0, Math.PI * 2,
+                    x => -3 * x * Math.Sin(0.75 * x) + Math.Exp(x) - 2 * x),
+                (0, 1,
+                    x => Math.Exp(3 * x) + 5 * Math.Exp(x) - 2 * x),
+                (0.5, 2.5, 
+                    x => 0.2 * x * Math.Log10(x) + (x - 2.3) * (x - 2.3))
+            };
+            
+            double epsilon = 1e-5;
+            OptimizationMethod[] methods =
+                {new DichotomyMethod(epsilon), new GoldenSectionMethod(epsilon), new FibonacciMethod(epsilon)};
+
+            foreach (var query in queries)
+            {
+                foreach (var method in methods)
+                {
+                    var result = method.FindMinimum(query.f, query.l, query.r);
+                    Console.WriteLine(method + ": X=" + result.X + " min Y=" + result.Y);
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
