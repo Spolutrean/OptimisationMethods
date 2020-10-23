92a85d8b202c535200d13bbccfd95a573ebab02d
Andrew Kashukov <andrei1999kashukov@gmail.com>
2020-10-23 23:02:02 +0300

    Add Brent's combined method implementation

--- HW1/HW1/Program.cs
+++ HW1/HW1/Program.cs
@@ -205,7 +205,121 @@ namespace HW1
             return "ParabolasMethod";
         }
     }
-    
+
+    public class BrentsCombinedMethod : OptimizationMethod
+    {
+        public BrentsCombinedMethod(double epsilon) : base(epsilon) { }
+
+        public override (double X, double Y) FindMinimum(Func<double, double> f, double l, double r)
+        {
+            double K = (3 - Math.Sqrt(5)) / 2;
+
+            double x, w, v, fx, fw, fv;
+            x = w = v = (r + l) / 2;
+            fx = fw = fv = f(x);
+
+            double d, e;
+            d = e = r - l;
+
+            while (Math.Abs(r - l - 2 * Epsilon) > Epsilon)
+            {
+                double g = e;
+                e = d;
+
+                double u;
+                if (x != w && x != v && w != v && fx != fw && fx != fv && fw != fv)
+                {
+                    double[] X = { x, w, v };
+                    double[] Y = { fx, fw, fv };
+
+                    for (int i = 0; i < 3; ++i)
+                    {
+                        for (int j = 0; j < 2; ++j)
+                        {
+                            if (X[j] > X[j + 1])
+                            {
+                                (X[j], X[j + 1]) = (X[j + 1], X[j]);
+                                (Y[j], Y[j + 1]) = (Y[j + 1], Y[j]);
+                            }
+                        }
+                    }
+
+                    double numerator = (X[1] - X[0]) * (X[1] - X[0]) * (Y[1] - Y[2]) - (X[1] - X[2]) * (X[1] - X[2]) * (Y[1] - Y[0]);
+                    double denominator = 2 * ((X[1] - X[0]) * (Y[1] - Y[2]) - (X[1] - X[2]) * (Y[1] - Y[0]));
+                    u = X[1] - numerator / denominator;
+
+                    if (u >= l + Epsilon && u <= r - Epsilon && Math.Abs(u - x) < g / 2)
+                    {
+                        d = Math.Abs(u - x);
+                    }
+                    else
+                    {
+                        u = GoldenSectionNext();
+                    }
+                }
+                else
+                {
+                    u = GoldenSectionNext();
+                }
+
+                if (Math.Abs(u - x) < Epsilon)
+                {
+                    u = x + Math.Sign(u - x) * Epsilon;
+                }
+
+                double fu = f(u);
+                UpdateInterval(ref l, ref r, x, u, fx, fu);
+                if (fu < fx)
+                {
+                    (v, w, x) = (w, x, u);
+                    (fv, fw, fx) = (fw, fx, fu);
+                }
+                else
+                {
+                    if (fu <= fw || w == x)
+                    {
+                        (v, w) = (w, u);
+                        (fv, fw) = (fw, fu);
+                    }
+                    else if (fu <= fv || v == x || v == w)
+                    {
+                        v = u;
+                        fv = fu;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                double GoldenSectionNext()
+                {
+                    double result;
+                    if (2 * x < r + l)
+                    {
+                        result = x + K * (r - x);
+                        d = r - x;
+                    }
+                    else
+                    {
+                        result = x - K * (x - l);
+                        d = x - l;
+                    }
+
+                    return result;
+                }
+            }
+
+            double answerX = x;
+            double answerY = f(answerX);
+            return (answerX, answerY);
+        }
+
+        public override string ToString()
+        {
+            return "BrentsCombinedMethod";
+        }
+    }
     public class Program
     {
         static void Main(string[] args)
@@ -217,9 +331,9 @@ namespace HW1
                 (6, 9.9,
                     x => Math.Pow(Math.Log10(x - 2), 2) + Math.Pow(Math.Log10(10 - x), 2) - Math.Pow(x, 0.2)),
                 (0, Math.PI * 2,
-                    x => -3 * x * Math.Sin(0.75 * x) + Math.Exp(x) - 2 * x),
+                    x => -3 * x * Math.Sin(0.75 * x) + Math.Exp(-2 * x)),
                 (0, 1,
-                    x => Math.Exp(3 * x) + 5 * Math.Exp(x) - 2 * x),
+                    x => Math.Exp(3 * x) + 5 * Math.Exp(-2 * x)),
                 (0.5, 2.5, 
                     x => 0.2 * x * Math.Log10(x) + (x - 2.3) * (x - 2.3))
             };
@@ -230,7 +344,8 @@ namespace HW1
                 new DichotomyMethod(epsilon), 
                 new GoldenSectionMethod(epsilon), 
                 new FibonacciMethod(epsilon), 
-                new ParabolasMethod(epsilon)
+                new ParabolasMethod(epsilon),
+                new BrentsCombinedMethod(epsilon)
             };
 
             foreach (var query in queries)
